183f6c2a0852d53f423060a73117fbe8b4a16354
Yousef201x <yousefmohamed2006x@gmail.com>
2024-08-21 19:54:52 +0300

    add Remove

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
+
+void welcome(){
+    system("Color 0e");
+    cout << endl;
+    cout << " @@       @@  @@@@@@@  @@      @@@@@@@  @@@@@@@@  @@@@    @@@@  @@@@@@@\n";
+    cout << " @@   @   @@  @@       @@      @@       @@    @@  @@  @  @  @@  @@\n";
+    cout << " @@  @@@  @@  @@@@@@   @@      @@       @@    @@  @@   @@   @@  @@@@@@\n";
+    cout << " @@ @@ @@ @@  @@       @@      @@       @@    @@  @@        @@  @@@\n";
+    cout << " @@@     @@@  @@@@@@@  @@@@@@  @@@@@@@  @@@@@@@@  @@        @@  @@@@@@@\n\n\n";
+    Sleep(6000);
+    system("cls");
+}
 
 int const avilableSlots = 1000 ;
 int booksID[avilableSlots] = {0};
 string booksTitle[avilableSlots];
 string booksAuthors[avilableSlots];
-bool booksStatus[avilableSlots];
+bool booksStatus[avilableSlots] {0};
 int currentPosition = 0 ;
 
 int getAvilableSlot(){
@@ -14,6 +27,10 @@ int getAvilableSlot(){
 
 int increaseCurrentPosition(){
     return currentPosition++ ;
+}
+
+int decreaseCurrentPosition(){
+    return currentPosition-- ;
 }
 
 void add(string title ,  string author){
@@ -32,6 +49,13 @@ bool isFound(int id){
     return booksID[--id] != 0;
 }
 
+void notFound(int id){
+    if(!isFound(id)){
+        cout << "Slot not found" ;
+        return;
+    }
+}
+
 void update(int id , string title , string author){
     int bookIndex = id - 1 ;
     booksTitle[bookIndex] = title ;
@@ -40,9 +64,21 @@ void update(int id , string title , string author){
 }
 
 void update(int id , string title){
+    notFound(id);
     int bookIndex = id - 1 ;
     booksTitle[bookIndex] = title ;
     cout << "Slot updated successfully\n";
+}
+
+void remove(int id){
+    notFound(id);
+    int bookIndex = id - 1 ;
+    booksID[bookIndex] = 0 ;
+    booksTitle[bookIndex] = '\0';
+    booksAuthors[bookIndex] = '\0' ;
+    booksStatus[bookIndex] = 0 ;
+    decreaseCurrentPosition() ;
+    cout << "Slot deleted successfully\n";
 }
 
 
